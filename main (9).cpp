cb629ec68eaa9908ffb35b72bc9881c85aec78f8
pumputis777 <facundopumputis@gmail.com>
2025-05-15 19:58:11 -0300

    Update main (9).cpp

--- main (9).cpp
+++ main (9).cpp
@@ -3,11 +3,14 @@
 using namespace std;
 int main()
 {
-    int num1;
-    float total;
-    cout << "este programa ve si un numero es par o inpar"<<endl;
+    int num1; // declaro la primera variable
+    float total; // declaro la segunda variable
+    
+    cout << "este programa ve si un numero es par o inpar"<<endl; //le indica al usuario lo que hace el programa
+    
     cout << "ingrese un numero: ";
     cin >> num1;
+    
     total = num1 % 2;
     if (total==0)
     {
