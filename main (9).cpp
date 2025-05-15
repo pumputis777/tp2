aa9fd12c5e852908209ba251abd879b897adf49d
pumputis777 <facundopumputis@gmail.com>
2025-05-15 20:01:23 -0300

    Update main (9).cpp

--- main (9).cpp
+++ main (9).cpp
@@ -7,16 +7,16 @@ int main()
     float total; // declaro la segunda variable
     
     cout << "este programa ve si un numero es par o inpar"<<endl; //le indica al usuario lo que hace el programa
-    
+    // Solicita al usuario que ingrese un numero
     cout << "ingrese un numero: ";
     cin >> num1;
     
-    total = num1 % 2;
-    if (total==0)
+    total = num1 % 2; // hace una operacion al num1 y guarda el resultado en la variable total
+    if (total==0) // verifica si lavariable total es igual 0
     {
     cout << num1 << " es un numero par";
     }
-    else
+    else //si no se cumplio la anterior funcion se ejecutara esto
     {
         cout << num1 << " es un numero inpar";
     }
